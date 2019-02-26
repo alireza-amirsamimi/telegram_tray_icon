db4fef08a075b97428b2431ef1691bb9ffb249ef
alireza-amirsamimi <alireza.amirsamimi@gmail.com>
2019-02-27 00:54:48 +0330

    generate.sh is edited. new icons are added.

--- generate.sh
+++ generate.sh
@@ -3,37 +3,54 @@ icon_file="telegram.svg"
 gray_file="telegram_gray.svg"
 red_file="telegram_red.svg"
 
-#creating a new folder
+# create a new folder
 rm -rf ./new 2>/dev/null
 mkdir  "new"
 
-#creating red icons 1-999
-for number in {1..999};do
-    #defining new name for file
-    new_name="ico_22_$number"
+echo -e "In some versions of Telegram,pattern of tray icon file name is:\n\n\n   1:\"ico_22_\"\n\nand in the other versions is\n\n\n   2:\"icon_22_\"\n"
+read -p "Which version are you using?" version
 
-#creating a copy from original file
+# create red icons 1-999
+for number in {1..999};do
+
+    # define new name for file
+    if [ $version == 1 ];then
+        new_name="ico_22_$number"
+    else
+        new_name="icon_22_$number"
+    fi
+
+# create a copy from original file
     cp "$red_file" new/"$new_name".svg
 
-#replacing No. 999 with $number in line 94  
+# replace No. 999 with $number in line 94  
     sed -i  "94s/999/$number/" new/"$new_name".svg
 
-#converting svg to png
+# convert svg to png
     inkscape -z -e new/"$new_name".png -w 22 -h 22 new/"$new_name".svg 2>/dev/null
 
-#removing svg file
+# remove svg file
     rm new/"$new_name".svg
 done
 
-#creating red icons _00 - _99
+# create red icons _00 - _99
 for counter in {1000..1099};do
     number=$(($counter - 1000))
+
     if [ $number -le 9 ];then
         number="0$number"
     fi
+
     number="_$number"
     number_string="_0"
-    new_name="ico_22_$counter"
+
+    if [ $version == 1 ];then
+        new_name="ico_22_$counter"
+    else
+        new_name="icon_22_$counter"
+    fi
+
+
 
     cp "$red_file" new/"$new_name".svg
 
@@ -45,33 +62,47 @@ for counter in {1000..1099};do
 
 done
 
-#creating gray icons 1-999
+# create gray icons 1-999
 for number in {1..999};do
-    #defining new name for file
-    new_name="icomute_22_$number"
+    # define new name for file
 
-#creating a copy from original file
+    if [ $version == 1 ];then
+        new_name="icomute_22_$number"
+    else
+        new_name="iconmute_22_$number"
+    fi
+
+
+# create a copy from original file
     cp "$gray_file" new/"$new_name".svg
 
-#replacing No. 999 with $number in line 94  
+# replace No. 999 with $number in line 94  
     sed -i  "94s/999/$number/" new/"$new_name".svg
 
-#converting svg to png
+# convert svg to png
     inkscape -z -e new/"$new_name".png -w 22 -h 22 new/"$new_name".svg 2>/dev/null
 
     rm new/"$new_name".svg
 
 done
 
-#creating gray icons _00 - _99
+# create gray icons _00 - _99
 for counter in {1000..1099};do
     number=$(($counter - 1000))
+
     if [ $number -le 9 ];then
         number="0$number"
     fi
+
     number="_$number"
     number_string="_0"
-    new_name="icomute_22_$counter"
+
+    if [ $version == 1 ];then
+        new_name="icomute_22_$counter"
+    else
+        new_name="iconmute_22_$counter"
+    fi
+
 
     cp "$gray_file" new/"$new_name".svg
 
@@ -83,6 +114,13 @@ for counter in {1000..1099};do
 
 done
 
-#creating default icon 
+if [ $version == 1 ];then
+    default_name="icomute_22_0"
+else
+    default_name="iconmute_22_0"
+fi
+
+
+# create default icon 
 inkscape -z -e  new/"icomute_22_0".png  -w 22 -h 22 "$icon_file" 2>/dev/null
 
